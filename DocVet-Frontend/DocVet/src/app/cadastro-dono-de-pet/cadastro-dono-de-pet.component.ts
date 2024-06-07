c6d72c63cca70fb14cd0181b0bee48215a2c119e
GGutiOS <guilherme.go.santos@gmail.com>
2024-06-07 18:39:29 -0300

    inserindo alguns html

--- DocVet-Frontend/DocVet/src/app/cadastro-dono-de-pet/cadastro-dono-de-pet.component.ts
+++ DocVet-Frontend/DocVet/src/app/cadastro-dono-de-pet/cadastro-dono-de-pet.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro-dono-de-pet',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,CommonModule,FormsModule],
   templateUrl: './cadastro-dono-de-pet.component.html',
   styleUrl: './cadastro-dono-de-pet.component.css'
 })
