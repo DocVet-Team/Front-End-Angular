39524cd73c897d7b4f9e77e17015ff1ad76a43d0
LeoAlves23 <117919882+LeoAlves23@users.noreply.github.com>
2024-06-10 01:07:49 -0300

    validações cadastro dono de pet

--- DocVet-Frontend/DocVet/src/app/cadastro-dono-de-pet/cadastro-dono-de-pet.component.ts
+++ DocVet-Frontend/DocVet/src/app/cadastro-dono-de-pet/cadastro-dono-de-pet.component.ts
@@ -1,23 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-import { MessagesModule } from 'primeng/messages';
 import { Message } from 'primeng/api';
-
+import { MessagesModule } from 'primeng/messages';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-cadastro-dono-de-pet',
   standalone: true,
-  imports: [RouterLink,CommonModule,FormsModule,ReactiveFormsModule,MessagesModule],
+  imports: [RouterLink, CommonModule, FormsModule, ReactiveFormsModule, MessagesModule, InputTextModule],
   templateUrl: './cadastro-dono-de-pet.component.html',
-  styleUrl: './cadastro-dono-de-pet.component.css'
+  styleUrls: ['./cadastro-dono-de-pet.component.css']
 })
-export class CadastroDonoDePetComponent {
-
+export class CadastroDonoDePetComponent implements OnInit {
   formulario!: FormGroup;
   messages: Message[] | undefined;
+
   constructor(private http: HttpClient, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
@@ -28,12 +28,16 @@ export class CadastroDonoDePetComponent {
       email: ['', [Validators.required, Validators.email]],
       senha: ['', [Validators.required, Validators.minLength(8)]],
       confsenha: ['', Validators.required]
-      
     });
     this.messages = [
-      { severity: 'success', detail: 'Info Message' }];
+      { severity: 'success', detail: 'Info Message' }
+    ];
   }
-  
+
+  isInvalid(controlName: string): boolean {
+    const control = this.formulario.get(controlName);
+    return control ? control.invalid && (control.dirty || control.touched) : false;
+  }
 
   submitForm(): void {
     if (this.formulario.valid) {
@@ -45,7 +49,6 @@ export class CadastroDonoDePetComponent {
             if (response) {
               // O cadastro foi realizado com sucesso
               alert("Cadastro realizado com sucesso!");
-              
             } else {
               // Ocorreu um erro durante o cadastro
               alert("Ocorreu um erro durante o cadastro. Tente novamente mais tarde.");
@@ -65,80 +68,219 @@ export class CadastroDonoDePetComponent {
   nameValidate(): void {
     const nomeControl = this.formulario.get('nome');
     if (nomeControl) {
-        // Lógica de validação do nome
-        // Por exemplo, verificar se o nome tem no mínimo 3 caracteres
-        if (nomeControl.value.length < 3) {
-            // Exibir mensagem de erro ou realizar outra ação
-        } else {
-            // Nome válido, realizar outra ação se necessário
-        }
+      // Lógica de validação do nome
+      if (nomeControl.value.length < 3) {
+        // Exibir mensagem de erro ou realizar outra ação
+      } else {
+        // Nome válido, realizar outra ação se necessário
+      }
     }
-}
+  }
 
-cpfValidate(): void {
+  cpfValidate(): void {
     const cpfControl = this.formulario.get('cpf');
     if (cpfControl) {
-        // Lógica de validação do CPF
-        // Por exemplo, verificar se o CPF tem 11 dígitos
-        if (cpfControl.value.length !== 11) {
-            // Exibir mensagem de erro ou realizar outra ação
-        } else {
-            // CPF válido, realizar outra ação se necessário
-        }
+      // Lógica de validação do CPF
+      if (cpfControl.value.length !== 11) {
+        // Exibir mensagem de erro ou realizar outra ação
+      } else {
+        // CPF válido, realizar outra ação se necessário
+      }
     }
-}
+  }
 
-celularValidate(): void {
+  celularValidate(): void {
     const telefoneControl = this.formulario.get('telefone');
     if (telefoneControl) {
-        // Lógica de validação do telefone
-        // Por exemplo, verificar se o telefone tem 11 dígitos
-        if (telefoneControl.value.length !== 11) {
-            // Exibir mensagem de erro ou realizar outra ação
-        } else {
-            // Telefone válido, realizar outra ação se necessário
-        }
+      // Lógica de validação do telefone
+      if (telefoneControl.value.length !== 11) {
+        // Exibir mensagem de erro ou realizar outra ação
+      } else {
+        // Telefone válido, realizar outra ação se necessário
+      }
     }
-}
+  }
 
-emailValidate(): void {
+  emailValidate(): void {
     const emailControl = this.formulario.get('email');
     if (emailControl) {
-        // Lógica de validação do email
-        // Por exemplo, verificar se o email é válido
-        if (!/\S+@\S+\.\S+/.test(emailControl.value)) {
-            // Exibir mensagem de erro ou realizar outra ação
-        } else {
-            // Email válido, realizar outra ação se necessário
-        }
+      // Lógica de validação do email
+      if (!/\S+@\S+\.\S+/.test(emailControl.value)) {
+        // Exibir mensagem de erro ou realizar outra ação
+      } else {
+        // Email válido, realizar outra ação se necessário
+      }
     }
-}
+  }
 
-mainPasswordValidate(): void {
+  mainPasswordValidate(): void {
     const senhaControl = this.formulario.get('senha');
     if (senhaControl) {
-        // Lógica de validação da senha principal
-        // Por exemplo, verificar se a senha tem no mínimo 8 caracteres
-        if (senhaControl.value.length < 8) {
-            // Exibir mensagem de erro ou realizar outra ação
-        } else {
-            // Senha principal válida, realizar outra ação se necessário
-        }
+      // Lógica de validação da senha principal
+      if (senhaControl.value.length < 8) {
+        // Exibir mensagem de erro ou realizar outra ação
+      } else {
+        // Senha principal válida, realizar outra ação se necessário
+      }
     }
-}
+  }
 
-comparePassword(): void {
+  comparePassword(): void {
     const senhaControl = this.formulario.get('senha');
     const confsenhaControl = this.formulario.get('confsenha');
     if (senhaControl && confsenhaControl) {
-        // Lógica de comparação de senha
-        // Por exemplo, verificar se as senhas são iguais
-        if (senhaControl.value !== confsenhaControl.value) {
-            // Exibir mensagem de erro ou realizar outra ação
-        } else {
-            // Senhas coincidem, realizar outra ação se necessário
-        }
+      // Lógica de comparação de senha
+      if (senhaControl.value !== confsenhaControl.value) {
+        // Exibir mensagem de erro ou realizar outra ação
+      } else {
+        // Senhas coincidem, realizar outra ação se necessário
+      }
     }
+  }
 }
 
-}
+
+// import { CommonModule } from '@angular/common';
+// import { HttpClient } from '@angular/common/http';
+// import { Component } from '@angular/core';
+// import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+// import { RouterLink } from '@angular/router';
+// import { MessagesModule } from 'primeng/messages';
+// import { Message } from 'primeng/api';
+// import { InputTextModule } from 'primeng/inputtext';
+
+
+// @Component({
+//   selector: 'app-cadastro-dono-de-pet',
+//   standalone: true,
+//   imports: [RouterLink,CommonModule,FormsModule,ReactiveFormsModule,MessagesModule,InputTextModule],
+//   templateUrl: './cadastro-dono-de-pet.component.html',
+//   styleUrl: './cadastro-dono-de-pet.component.css'
+// })
+// export class CadastroDonoDePetComponent {
+
+//   formulario!: FormGroup;
+//   messages: Message[] | undefined;
+//   constructor(private http: HttpClient, private formBuilder: FormBuilder) { }
+
+//   ngOnInit(): void {
+//     this.formulario = this.formBuilder.group({
+//       nome: ['', [Validators.required, Validators.minLength(3)]],
+//       cpf: ['', [Validators.required, Validators.minLength(11)]],
+//       telefone: ['', [Validators.required, Validators.minLength(11)]],
+//       email: ['', [Validators.required, Validators.email]],
+//       senha: ['', [Validators.required, Validators.minLength(8)]],
+//       confsenha: ['', Validators.required]
+
+//     });
+//     this.messages = [
+//       { severity: 'success', detail: 'Info Message' }];
+//   }
+
+
+//   submitForm(): void {
+//     if (this.formulario.valid) {
+//       // Faça a solicitação para a sua API usando HttpClient
+//       this.http.post('http://localhost:8080/api/v1/dono-pets/', this.formulario.value)
+//         .subscribe(
+//           response => {
+//             // Verifique o status da resposta da API
+//             if (response) {
+//               // O cadastro foi realizado com sucesso
+//               alert("Cadastro realizado com sucesso!");
+//             } else {
+//               // Ocorreu um erro durante o cadastro
+//               alert("Ocorreu um erro durante o cadastro. Tente novamente mais tarde.");
+//             }
+//           },
+//           error => {
+//             // Ocorreu um erro de conexão com a API
+//             alert("Ocorreu um erro de conexão com a API. Verifique sua conexão de internet e tente novamente.");
+//           }
+//         );
+//     } else {
+//       // Formulário inválido
+//       alert("Por favor, preencha todos os campos corretamente.");
+//     }
+//   }
+
+//   nameValidate(): void {
+//     const nomeControl = this.formulario.get('nome');
+//     if (nomeControl) {
+//         // Lógica de validação do nome
+//         // Por exemplo, verificar se o nome tem no mínimo 3 caracteres
+//         if (nomeControl.value.length < 3) {
+//             // Exibir mensagem de erro ou realizar outra ação
+//         } else {
+//             // Nome válido, realizar outra ação se necessário
+//         }
+//     }
+// }
+
+// cpfValidate(): void {
+//     const cpfControl = this.formulario.get('cpf');
+//     if (cpfControl) {
+//         // Lógica de validação do CPF
+//         // Por exemplo, verificar se o CPF tem 11 dígitos
+//         if (cpfControl.value.length !== 11) {
+//             // Exibir mensagem de erro ou realizar outra ação
+//         } else {
+//             // CPF válido, realizar outra ação se necessário
+//         }
+//     }
+// }
+
+// celularValidate(): void {
+//     const telefoneControl = this.formulario.get('telefone');
+//     if (telefoneControl) {
+//         // Lógica de validação do telefone
+//         // Por exemplo, verificar se o telefone tem 11 dígitos
+//         if (telefoneControl.value.length !== 11) {
+//             // Exibir mensagem de erro ou realizar outra ação
+//         } else {
+//             // Telefone válido, realizar outra ação se necessário
+//         }
+//     }
+// }
+
+// emailValidate(): void {
+//     const emailControl = this.formulario.get('email');
+//     if (emailControl) {
+//         // Lógica de validação do email
+//         // Por exemplo, verificar se o email é válido
+//         if (!/\S+@\S+\.\S+/.test(emailControl.value)) {
+//             // Exibir mensagem de erro ou realizar outra ação
+//         } else {
+//             // Email válido, realizar outra ação se necessário
+//         }
+//     }
+// }
+
+// mainPasswordValidate(): void {
+//     const senhaControl = this.formulario.get('senha');
+//     if (senhaControl) {
+//         // Lógica de validação da senha principal
+//         // Por exemplo, verificar se a senha tem no mínimo 8 caracteres
+//         if (senhaControl.value.length < 8) {
+//             // Exibir mensagem de erro ou realizar outra ação
+//         } else {
+//             // Senha principal válida, realizar outra ação se necessário
+//         }
+//     }
+// }
+
+// comparePassword(): void {
+//     const senhaControl = this.formulario.get('senha');
+//     const confsenhaControl = this.formulario.get('confsenha');
+//     if (senhaControl && confsenhaControl) {
+//         // Lógica de comparação de senha
+//         // Por exemplo, verificar se as senhas são iguais
+//         if (senhaControl.value !== confsenhaControl.value) {
+//             // Exibir mensagem de erro ou realizar outra ação
+//         } else {
+//             // Senhas coincidem, realizar outra ação se necessário
+//         }
+//     }
+// }
+
+// }
--- DocVet-Frontend/DocVet/src/app/cadastro-veterinario/cadastro-veterinario.component.ts
+++ DocVet-Frontend/DocVet/src/app/cadastro-veterinario/cadastro-veterinario.component.ts
@@ -9,57 +9,5 @@ import { VeterinarioService } from '../services/veterinarios.service';
   templateUrl: './cadastro-veterinario.component.html',
   styleUrl: './cadastro-veterinario.component.css'
 })
-export class CadastroVeterinarioComponent implements OnInit{
-
-  lista: Veterinario[] = [];
-  listaSel: Veterinario[] | undefined;
-  obj: Veterinario = new Veterinario();
-  mens = '';
-
-  constructor(private api: VeterinarioService){}
-
-
-  ngOnInit(): void {
-    this.consultar();
-    this.consultarSel();
-  }
-
-  consultarSel() {
-    this.api.consultar()
-    .toPromise()
-    .then
-    (res => {
-      this.listaSel = res;
-    });
-  }
-
-  consultar() {
-    this.api.consultar()
-    .toPromise()
-    .then
-    ((res: any) => {
-      this.lista = res;
-    });
-  }
-
-  adicionar(){
-    this.api.adicionar(this.obj)
-    .toPromise()
-    .then((veterinario: Veterinario |any) => {
-      this.mens = veterinario.nome + "foi adicionado(a) com sucesso!";
-    });
-  }
-
-  excluir(){
-    this.api.excluir(this.obj.id)
-    .toPromise()
-    .then(() => {
-      this.mens = "Pessoa excluída com sucesso!";
-      this.consultar();
-    })
-  }
-
-  alterar(){
-    this.api.alterar(this.obj, this.obj)
-  }
+export class CadastroVeterinarioComponent {
 }
