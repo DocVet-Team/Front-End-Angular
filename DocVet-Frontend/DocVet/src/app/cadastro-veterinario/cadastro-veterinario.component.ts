913c8e263c712360957e899868f6498156e22d42
LeoAlves23 <117919882+LeoAlves23@users.noreply.github.com>
2024-06-10 03:43:03 -0300

    correção das linhas com erro no cadastro de veterinário.component

--- DocVet-Frontend/DocVet/src/app/cadastro-veterinario/cadastro-veterinario.component.ts
+++ DocVet-Frontend/DocVet/src/app/cadastro-veterinario/cadastro-veterinario.component.ts
@@ -1,27 +1,21 @@
-import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
-import { Message } from 'primeng/api';
-import { MessagesModule } from 'primeng/messages';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Veterinario } from '../models/veterinario.model';
+import { VeterinarioService } from '../services/veterinarios.service';
 
 @Component({
   selector: 'app-cadastro-veterinario',
-  standalone: true,
-  imports: [RouterLink, CommonModule, FormsModule, ReactiveFormsModule, MessagesModule],
   templateUrl: './cadastro-veterinario.component.html',
-  styleUrl: './cadastro-veterinario.component.css'
+  styleUrls: ['./cadastro-veterinario.component.css']
 })
-export class CadastroVeterinarioComponent implements OnInit{
+export class CadastroVeterinarioComponent implements OnInit {
 
   lista: Veterinario[] = [];
   listaSel: Veterinario[] | undefined;
   obj: Veterinario = new Veterinario();
   mens = '';
 
-  constructor(private api: VeterinarioService){}
-
+  constructor(private api: VeterinarioService) {}
 
   ngOnInit(): void {
     this.consultar();
@@ -30,40 +24,38 @@ export class CadastroVeterinarioComponent implements OnInit{
 
   consultarSel() {
     this.api.consultar()
-    .toPromise()
-    .then
-    (res => {
-      this.listaSel = res;
-    });
+      .toPromise()
+      .then((res: any) => {
+        this.listaSel = res;
+      });
   }
 
   consultar() {
     this.api.consultar()
-    .toPromise()
-    .then
-    ((res: any) => {
-      this.lista = res;
-    });
+      .toPromise()
+      .then((res: any) => {
+        this.lista = res;
+      });
   }
 
-  adicionar(){
+  adicionar() {
     this.api.adicionar(this.obj)
-    .toPromise()
-    .then((veterinario: Veterinario |any) => {
-      this.mens = veterinario.nome + "foi adicionado(a) com sucesso!";
-    });
+      .toPromise()
+      .then((veterinario: Veterinario | any) => {
+        this.mens = `${veterinario.nome} foi adicionado(a) com sucesso!`;
+      });
   }
 
-  excluir(){
+  excluir() {
     this.api.excluir(this.obj.id)
-    .toPromise()
-    .then(() => {
-      this.mens = "Pessoa excluída com sucesso!";
-      this.consultar();
-    })
+      .toPromise()
+      .then(() => {
+        this.mens = 'Pessoa excluída com sucesso!';
+        this.consultar();
+      });
   }
 
-  alterar(){
-    this.api.alterar(this.obj, this.obj)
+  alterar() {
+    this.api.alterar(this.obj.id, this.obj);
   }
 }
